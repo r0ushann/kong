21bafe28d5a8bf9cf3793a9e1cadd653208a57a2
Mayo <i@shoujo.io>
2022-10-08 03:47:24 +0000

    add test spec for otel data race

--- spec/03-plugins/37-opentelemetry/04-exporter_spec.lua
+++ spec/03-plugins/37-opentelemetry/04-exporter_spec.lua
@@ -3,12 +3,16 @@ local helpers = require "spec.helpers"
 local utils = require "kong.tools.utils"
 local tablex = require "pl.tablex"
 local pb = require "pb"
+local pl_file = require "pl.file"
+local ngx_re = require "ngx.re"
 
 local table_merge = utils.table_merge
 local HTTP_PORT = 35000
 
 for _, strategy in helpers.each_strategy() do
   describe("opentelemetry exporter #" .. strategy, function()
+    local bp
+
     lazy_setup(function ()
       -- overwrite for testing
       pb.option("enum_as_value")
@@ -22,13 +26,7 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     -- helpers
-    local function setup_instrumentations(types, config)
-      local bp, _ = assert(helpers.get_db_utils(strategy, {
-        "services",
-        "routes",
-        "plugins",
-      }, { "opentelemetry" }))
-
+    local function setup_instrumentations(types, config, fixtures)
       local http_srv = assert(bp.services:insert {
         name = "mock-service",
         host = helpers.mock_upstream_host,
@@ -42,17 +40,18 @@ for _, strategy in helpers.each_strategy() do
       bp.plugins:insert({
         name = "opentelemetry",
         config = table_merge({
-          endpoint = "http://127.0.0.1:" .. HTTP_PORT,
+          endpoint = "http://127.0.0.1:8765",
           batch_flush_delay = -1, -- report immediately
         }, config)
       })
 
-      assert(helpers.start_kong {
+      assert(helpers.start_kong({
+        proxy_listen = "0.0.0.0:9000",
         database = strategy,
         nginx_conf = "spec/fixtures/custom_nginx.template",
         plugins = "opentelemetry",
         opentelemetry_tracing = types,
-      })
+      }, nil, nil, fixtures))
     end
 
     describe("valid #http request", function ()
@@ -73,7 +72,7 @@ for _, strategy in helpers.each_strategy() do
         local headers, body
         helpers.wait_until(function()
           local thread = helpers.http_server(HTTP_PORT, { timeout = 10 })
-          local cli = helpers.proxy_client(7000)
+          local cli = helpers.proxy_client(7000, 9000)
           local r = assert(cli:send {
             method  = "GET",
             path    = "/",
@@ -177,6 +176,103 @@ for _, strategy in helpers.each_strategy() do
 
         local scope_spans = decoded.resource_spans[1].scope_spans
         assert.is_true(#scope_spans > 0, scope_spans)
+      end)
+    end)
+
+    describe("data #race with cascaded multiple spans", function ()
+      lazy_setup(function()
+        bp, _ = assert(helpers.get_db_utils(strategy, {
+          "services",
+          "routes",
+          "plugins",
+        }, { "opentelemetry" }))
+
+        pl_file.delete("/tmp/kong_opentelemetry_data")
+
+        local fixtures = {
+          http_mock = {}
+        }
+
+        fixtures.http_mock.my_server_block = [[
+          server {
+            server_name myserver;
+            listen 8765;
+            client_body_buffer_size 1024k;
+
+            location / {
+              content_by_lua_block {
+                ngx.req.read_body()
+                local data = ngx.req.get_body_data()
+
+                local fd = assert(io.open("/tmp/kong_opentelemetry_data", "a"))
+                assert(fd:write(ngx.encode_base64(data)))
+                assert(fd:write("\n")) -- ensure last line ends in newline
+                assert(fd:close())
+
+                return 200;
+              }
+            }
+          }
+        ]]
+
+        for i = 1, 5 do
+          local svc = assert(bp.services:insert {
+            host = "127.0.0.1",
+            port = 9000,
+            path = i == 1 and "/" or ("/cascade-" .. (i - 1)),
+          })
+
+          bp.routes:insert({ service = svc,
+                             protocols = { "http" },
+                             paths = { "/cascade-" .. i },
+                             strip_path = true })
+        end
+
+        setup_instrumentations("request", {}, fixtures)
+      end)
+
+      lazy_teardown(function()
+        -- pl_file.delete("/tmp/kong_opentelemetry_data")
+        helpers.kill_http_server(HTTP_PORT)
+        helpers.stop_kong()
+      end)
+
+      it("works", function ()
+        local pb_set = {}
+        local cli = helpers.proxy_client(7000, 9000)
+        local r = assert(cli:send {
+          method  = "GET",
+          path    = "/cascade-5",
+        })
+        assert.res_status(200, r)
+
+        -- close client connection
+        cli:close()
+
+        helpers.wait_until(function()
+          local fd, err = io.open("/tmp/kong_opentelemetry_data", "r")
+          if err then
+            return false, "failed to open file: " .. err
+          end
+
+          local body = fd:read("*a")
+          pb_set = ngx_re.split(body, "\n")
+
+          print(#pb_set)
+          local count = 0
+          for _, pb_data in ipairs(pb_set) do
+            local decoded = assert(pb.decode("opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest", ngx.decode_base64(pb_data)))
+            assert.not_nil(decoded)
+            -- print(require("inspect")(decoded))
+            count = count + #decoded.resource_spans[1].scope_spans
+          end
+
+          if count < 5 then
+            return false, "not enough spans: " .. count
+          end
+
+          return true
+        end, 60)
       end)
     end)
 
